b591b76e490554608074063988794fbe4ff1e9e0
mootaz20 <mootazalhalak695@gmail.com>
2024-04-16 00:19:31 +0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Educational-Dashboard'`, () => {
+  it(`should have as title 'EducationalDashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Educational-Dashboard');
+    expect(app.title).toEqual('EducationalDashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Educational-Dashboard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('EducationalDashboard app is running!');
   });
 });
